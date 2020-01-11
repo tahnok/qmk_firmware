fabe1235faeefa6bc5215330be264665ded0a36c
Wesley Ellis <tahnok@gmail.com>
2020-01-11 14:27:06 -0500

    works!

--- keyboards/2fer/2fer.h
+++ keyboards/2fer/2fer.h
@@ -26,10 +26,10 @@
  * The second converts the arguments into a two-dimensional array which
  * represents the switch matrix.
  */
-#define LAYOUT( \
-    k00, k01 \
-) \
-{ \
-    { k00 }, \
-    { k01 }  \
-}
+/* #define LAYOUT( \
+//     k00, k01 \
+// ) \
+// { \
+//     { k00, k01 } \
+// }
+*/
