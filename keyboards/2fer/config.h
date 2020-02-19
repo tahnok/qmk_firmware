89bf7e4ee11a1e890000c16864a14441ac61c338
Wesley Ellis <tahnok@gmail.com>
2020-02-18 20:55:03 -0500

    With PCB\!

--- keyboards/2fer/config.h
+++ keyboards/2fer/config.h
@@ -41,8 +41,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
  *
  */
-#define MATRIX_COL_PINS { D6, B7 }
-#define MATRIX_ROW_PINS { D7 }
+#define MATRIX_COL_PINS { B6, B7 }
+#define MATRIX_ROW_PINS { D6 }
 #define UNUSED_PINS
 
 /* COL2ROW, ROW2COL*/
--- keyboards/2fer/keymaps/default/keymap.c
+++ keyboards/2fer/keymaps/default/keymap.c
@@ -2,6 +2,7 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {
-    { KC_A, KC_D }
+    // { KC_PGDOWN, KC_PGUP }
+    { KC_SYSTEM_SLEEP, LCAG(KC_A)}
   }
 };
